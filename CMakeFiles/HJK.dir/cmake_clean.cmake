7255206fb72a14b77bbf24c070956571f1c95250
JQKA233 <852576001@qq.com>
2024-03-14 01:05:45 +0800

    2

---
+++ CMakeFiles/HJK.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HJK.dir/LaneLineDetect.cpp.o"
+  "HJK"
+  "HJK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HJK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readme.md
@@ -0,0 +1,19 @@
+###
+##
+```bash
+sudo apt install cmake
+
+sudo apt install libopencv-dev
+
+sudo apt install build-essential
+
+git clone https://github.com/JQKA233/CarLaneTest.git
+
+cd CarLaneTest
+
+cmake .
+
+make
+
+./HJK
+```
